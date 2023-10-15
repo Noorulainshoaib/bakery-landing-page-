4a977c4d27be1cf723771b93b0993ad332e7417a
Your Name <ulainn400@gmail.com>
2023-10-15 19:25:42 +0500

    done

---
+++ index.js
@@ -0,0 +1,105 @@
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-bar').onclick=() =>{
+    navbar.classList.toggle('active');
+}
+
+
+
+let search = document.querySelector('.search');
+document.querySelector('#search').onclick=() =>{
+    search.classList.toggle('active');
+}
+
+const typingH2 = document.getElementById("typing-h2");
+    const textToType = "Delicious Bakery Items for Everyone";
+    let currentText = "";
+    let textIndex = 0;
+
+    function typeText() {
+        if (textIndex < textToType.length) {
+            currentText += textToType.charAt(textIndex);
+            typingH2.innerText = currentText;
+            textIndex++;
+            setTimeout(typeText, 100); // Adjust the typing speed here (100ms delay in this example)
+        }
+    }
+
+    // Call the typing animation function
+    typeText();
+var swiper = new Swiper(".product-row", {
+    spaceBetween: 30,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+var swiper = new Swiper(".blogs-row", {
+    spaceBetween: 30,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation:{
+        nextE1 :".swiper-button-next",
+        prevE1 :".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 1,
+      },
+      1024: {
+        slidesPerView: 1,
+      },
+    },
+  });
+
+  var swiper = new Swiper(".review-row", {
+    spaceBetween: 30,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
